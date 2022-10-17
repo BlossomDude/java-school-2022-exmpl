76e647911f4a2f5a5705798ceb88ec222c0b0945
BlossomDude <97163421+BlossomDude@users.noreply.github.com>
2022-10-17 19:59:10 +0300

    Add files via upload

---
+++ src/ru/croc/task2/Task2.java
@@ -0,0 +1,45 @@
+package ru.croc.task2;
+import java.util.Scanner;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);                                          //Инициализируем сканнер
+        double byteInput = in.nextDouble();                                           //Присвоим входные данные
+
+
+        convert(byteInput);
+    }
+
+    public static void convert(double byteInput){                                     //Метод для конвертации байтов
+
+        int grade = 0;                                                                //Инициализируем грейд (разрядность)
+
+        while (byteInput >= 1024){                                                    //Цикл для определения разрядности
+            byteInput /= 1024;                                                        //и вычисления
+            grade++;
+        }
+
+        switch (grade){                                                               //Перебираем в порядке возрастания
+            case 0:                                                                   //разрядность и исходя из этого
+                System.out.println(String.format("%.1f", byteInput) + " B");          //выводим ответ
+                break;
+            case 1:
+                System.out.println(String.format("%.1f", byteInput) + " KB");
+                break;
+            case 2:
+                System.out.println(String.format("%.1f", byteInput) + " MB");
+                break;
+            case 3:
+                System.out.println(String.format("%.1f", byteInput) + " MB");
+                break;
+            case 4:
+                System.out.println(String.format("%.1f", byteInput) + " GB");
+                break;
+            case 5:
+                System.out.println(String.format("%.1f", byteInput) + " TB");
+                break;
+        }
+    }
+}
